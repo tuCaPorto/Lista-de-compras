0f445fc7a45e3c8c195f9dbe022cf5e0b5c1f3ad
tuCaPorto <121845875+tuCaPorto@users.noreply.github.com>
2023-09-11 14:42:59 -0300

    Add files via upload

---
+++ Lista de compras/aula26.py
@@ -0,0 +1,32 @@
+import os
+
+lista = []
+
+while True:
+    print("Selecione uma opção")
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('clear')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input('Escolha o indice para apagar: ')
+        
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor, digite um numero inteiro.')
+        except IndexError:
+            print('Indice não exista na lista.')
+    elif opcao == 'l':
+        os.system('clear')
+
+        if len(lista) == 0:
+            print('Nada para listar.')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+    else:
+        print('Por favor, escolha i, a ou l.')
